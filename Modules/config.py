21f8fecab32f10ca89cd94b003745759c4f0ea25
shivanshisondhiya25-cpu <shivanshisondhiya25@gmail.com>
2026-02-06 19:14:40 +0530

    Update config.py

--- Modules/config.py
+++ Modules/config.py
@@ -11,23 +11,23 @@ load_dotenv()
 class Config(object):
 
     # Your API HASH
-    API_HASH = os.environ.get('API_HASH')
+    API_HASH = os.environ.get('ed80c99568c99f73e8b739f09771bd3b')
 
     # Your API ID
-    APP_ID = int(os.environ.get('APP_ID'))
+    APP_ID = int(os.environ.get('32468786')
 
     # Your Bot Token
-    BOT_TOKEN = os.environ.get('BOT_TOKEN')
+    BOT_TOKEN = os.environ.get('8240655341:AAFNqdnLuPMj778MkBLWoN0vQiYuZ6z9vW8')
 
     # Your Telegram ID (optional)
-    OWNER_ID = os.environ.get('OWNER_ID')
+    OWNER_ID = os.environ.get('5501488547')
 
     # Upload method (default to False)
     AS_ZIP = bool(strtobool(os.environ.get('AS_ZIP', 'False')))
 
     # Channel/Group ID where you dump all the downloaded files.
-    DUMP_ID = int(os.environ.get('DUMP_ID')
-                  ) if os.environ.get('DUMP_ID') else None
+    DUMP_ID = int(os.environ.get('1003860418822')
+                  ) if os.environ.get('1003860418822') else None
 
     PLUGINS = {'root': 'Bot.plugins'}
 
